d9103ace74c4d6b99920ff3292da940aecfa6c35
kulczykszymon <szymon.kulczyk@student.wat.edu.pl>
2026-04-16 13:26:19 +0200

    wip: complex data struture

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 
 users: list = [
     {'username': 'oliwia' , 'location':'łódź','posts':1,'usermessage':['{życzenia1', 'kochamlegie1' , 'sprzedam opla', 'kiwi1']},
-    {'username': 'paweł' , 'location':'ostroda','posts':2,'usermessage':['życzenia2', 'kochamlegie2' , 'sprzedam opla', 'kiwi2']},
+    {'username': 'paweł' , 'location':'ostroda','posts':2,'usermessage':['życzenia2', 'kochamlegie2' , 'sprzedam opla1']},
     {'username': 'eliza' , 'location':'radom','posts':3,'usermessage':['życzenia3', 'kochamlegie3']},
-    {'username': 'filip' , 'location':'dęblin','posts':4,'usermessage':['życzenia4', 'kochamlegie4' , 'sprzedam opla', 'kiwi4']},
+    {'username': 'filip' , 'location':'dęblin','posts':4,'usermessage':['życzenia4', 'kochamlegie4' , 'sprzedam opla3', 'kiwi4']},
 ]
 
 for user in users[1:]:
