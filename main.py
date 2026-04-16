d63b3c05679f99d6c9331b2a7e9611334838a1d8
kulczykszymon <szymon.kulczyk@student.wat.edu.pl>
2026-04-16 13:23:42 +0200

    wip: complex data struture

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
 
 users: list = [
-    {'username': 'oliwia' , 'location':'łódź','posts':1,'usermessage':['{życzenia1', 'kochamlegie' , 'sprzedam opla', 'kiwi1']},
-    {'username': 'paweł' , 'location':'ostroda','posts':2,'usermessage':['życzenia2', 'kochamlegie' , 'sprzedam opla', 'kiwi1']},
-    {'username': 'eliza' , 'location':'radom','posts':3,'usermessage':['życzenia3', 'kochamlegie' , 'sprzedam opla', 'kiwi1']},
-    {'username': 'filip' , 'location':'dęblin','posts':4,'usermessage':['życzenia4', 'kochamlegie' , 'sprzedam opla', 'kiwi1']},
+    {'username': 'oliwia' , 'location':'łódź','posts':1,'usermessage':['{życzenia1', 'kochamlegie1' , 'sprzedam opla', 'kiwi1']},
+    {'username': 'paweł' , 'location':'ostroda','posts':2,'usermessage':['życzenia2', 'kochamlegie2' , 'sprzedam opla', 'kiwi2']},
+    {'username': 'eliza' , 'location':'radom','posts':3,'usermessage':['życzenia3', 'kochamlegie3']},
+    {'username': 'filip' , 'location':'dęblin','posts':4,'usermessage':['życzenia4', 'kochamlegie4' , 'sprzedam opla', 'kiwi4']},
 ]
 
 for user in users[1:]:
-    print(f'twój znajomy {user['username']} z miejscowości [{user["location"]} opublikował {user["posts"]} wiadomości o treści. Ostatnia wiadomość {user['usermessages']}')
+    print(f'twój znajomy {user['username']} z miejscowości {user["location"]} opublikował {user["posts"]} wiadomości o treści. Ostatnia wiadomość {user['usermessage'] [-1]}')
     
 #     twoj znajomy filip z miejscowości Dęblin opublikował 1 post o treści: życzenia
