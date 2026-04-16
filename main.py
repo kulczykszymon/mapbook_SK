dac168dd96bd21ad73cb371355be792b49d26d27
kulczykszymon <szymon.kulczyk@student.wat.edu.pl>
2026-04-16 12:41:53 +0200

    wip: simple structure of users list

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Geodezja i geoinformacya rządzi111')
+zmienna_1: str = 'oliwia'
+zmienna_2: str = 'paweł'
+zmienna_3: str = 'eliza'
+zmienna_4: str = 'filip'
+zmienna_5: str = 'ewelina'
+zmienna_6: str = 'weronika'
+print(zmienna_1, zmienna_2)
+print(zmienna_3)
+print(zmienna_4)
+print(zmienna_5)
+print(zmienna_6)
