83b9b30438fe7fb39c2a33bdd9662ed80ed20989
kulczykszymon <szymon.kulczyk@student.wat.edu.pl>
2026-04-16 12:53:50 +0200

    wip: simple structure of users list

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ zmienna_3: str = 'eliza'
 zmienna_4: str = 'filip'
 zmienna_5: str = 'ewelina'
 zmienna_6: str = 'weronika'
-print(zmienna_1, zmienna_2)
-print(zmienna_3)
-print(zmienna_4)
-print(zmienna_5)
-print(zmienna_6)
+
+users: list = [zmienna_1, zmienna_2, zmienna_3, zmienna_4, zmienna_5, zmienna_6]
+
+for user in users:
+    print(user)
