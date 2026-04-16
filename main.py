a5e2f8a113de25fdae40b1acad16b5dde8bafd65
kulczykszymon <szymon.kulczyk@student.wat.edu.pl>
2026-04-16 13:19:46 +0200

    wip: complex data struture

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-zmienna_1: str = 'oliwia'
-zmienna_2: str = 'paweł'
-zmienna_3: str = 'eliza'
-zmienna_4: str = 'filip'
-zmienna_5: str = 'ewelina'
-zmienna_6: str = 'weronika'
 
-users: list = [zmienna_1, zmienna_2, zmienna_3, zmienna_4, zmienna_5, zmienna_6]
 
-for user in users:
-    print(user)
+users: list = [
+    {'username': 'oliwia' , 'location':'łódź','posts':1,'usermessage':['{życzenia1', 'kochamlegie' , 'sprzedam opla', 'kiwi1']},
+    {'username': 'paweł' , 'location':'ostroda','posts':2,'usermessage':['życzenia2', 'kochamlegie' , 'sprzedam opla', 'kiwi1']},
+    {'username': 'eliza' , 'location':'radom','posts':3,'usermessage':['życzenia3', 'kochamlegie' , 'sprzedam opla', 'kiwi1']},
+    {'username': 'filip' , 'location':'dęblin','posts':4,'usermessage':['życzenia4', 'kochamlegie' , 'sprzedam opla', 'kiwi1']},
+]
+
+for user in users[1:]:
+    print(f'twój znajomy {user['username']} z miejscowości [{user["location"]} opublikował {user["posts"]} wiadomości o treści. Ostatnia wiadomość {user['usermessages']}')
+    
+#     twoj znajomy filip z miejscowości Dęblin opublikował 1 post o treści: życzenia
